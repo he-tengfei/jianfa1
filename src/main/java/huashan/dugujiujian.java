88eb5828187c7373a3e7241bde88fb16a6df1d2b
hetengfei <2711825945@qq.com>
2020-12-14 20:15:07 +0800

    创建剑法项目

---
+++ src/main/java/huashan/dugujiujian.java
@@ -0,0 +1,13 @@
+package huashan;
+
+
+public class dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("快乐西天");
+        System.out.println("快乐西天");
+        System.out.println("快乐西天");
+        System.out.println("快乐西天");
+
+
+    }
+}
