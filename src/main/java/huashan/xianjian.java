0a71e9fbf701002df2657ae6a78b60af1c9789fc
hetengfei <2711825945@qq.com>
2020-12-14 23:48:40 +0800

    fdxtrt

--- src/main/java/huashan/xianjian.java
+++ src/main/java/huashan/xianjian.java
@@ -4,5 +4,7 @@ public class xianjian {
     public static void main(String[] args) {
         System.out.println("xinajian");
         System.out.println("xinajian");
+        System.out.println("xinajian");
+        System.out.println("xinajian");
     }
 }
